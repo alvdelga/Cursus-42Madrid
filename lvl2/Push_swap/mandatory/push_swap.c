ba371210825801d1d8ec01a1de8b54a9507cb0b8
Leam Vaut <leam@localhost.localdomain>
2025-02-21 21:34:41 +0100

    clean

--- lvl2/Push_swap/mandatory/push_swap.c
+++ lvl2/Push_swap/mandatory/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: alvdelga <alvdelga@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 13:45:16 by alvdelga          #+#    #+#             */
-/*   Updated: 2025/02/21 18:16:43 by alvdelga         ###   ########.fr       */
+/*   Updated: 2025/02/21 19:56:35 by alvdelga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "push_swap.h"
-
+/*
 int	main(int argc, char *argv[])
 {
 	long	*numbers;
@@ -38,3 +38,54 @@ int	main(int argc, char *argv[])
 	free_stack(&a);
 	return (0);
 }
+*/
+
+#include "push_swap.h"
+
+int	validate_args(int argc, char *argv[])
+{
+	int	j;
+	
+	j = 1;
+	while (j < argc)
+	{
+		if (argv[j][0] == '\0')
+		{
+			write(2, "Error\n", 6);
+			return (1);
+		}
+		j++;
+	}
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	long	*numbers;
+	int		i;
+	t_stack	*a;
+	t_stack	*b;
+
+	i = 0;
+	a = NULL;
+	b = NULL;
+
+	if (argc < 2)
+		return (0);
+	if (validate_args(argc, argv))
+		return (1);
+	numbers = initialize(argc, argv);
+	if (!numbers)
+		return (1);
+	while (numbers[i] != LONG_MAX)
+		fill_stack(&a, numbers[i++]);
+	free(numbers);
+
+	if (!(check_sort(&a)))
+		sort(&a, &b);
+
+	free_stack(&a);
+	return (0);
+}
+
+
