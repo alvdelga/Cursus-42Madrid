dd118da7a1b171646e28311565b440d181755213
Alvaro Delgado <alvdelga@student.42madrid.com>
2025-02-26 15:35:46 +0100

    rendu

--- lvl2/42-EXAM/.system/grading/tester.sh
+++ lvl2/42-EXAM/.system/grading/tester.sh
@@ -3,47 +3,20 @@
 #                                                         :::      ::::::::    #
 #    tester.sh                                          :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+         #
+#    By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/06/20 02:26:11 by jcluzet           #+#    #+#              #
-#    Updated: 2022/09/01 23:45:41 by jcluzet          ###   ########.fr        #
+#    Created: 2022/11/06 23:07:59 by pandalaf          #+#    #+#              #
+#    Updated: 2022/11/07 00:37:40 by pandalaf         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-FILE='lcm.c'
-ASSIGN='lcm'
+FILE='flood_fill.c'
+ASSIGN='flood_fill'
 
-bash .system/auto_correc_main.sh $FILE $ASSIGN "1" "2"
+bash .system/auto_correc_main.sh $FILE $ASSIGN
 if [ -e .system/grading/traceback ];then
-    mv .system/grading/traceback .
+	mv .system/grading/traceback .
 	exit 1
 fi
-
-bash .system/auto_correc_main.sh $FILE $ASSIGN "3" "8"
-if [ -e .system/grading/traceback ];then
-    mv .system/grading/traceback .
-	exit 1
-fi
-
-bash .system/auto_correc_main.sh $FILE $ASSIGN "-1" "2932"
-if [ -e .system/grading/traceback ];then
-    mv .system/grading/traceback .
-	exit 1
-fi
-
-bash .system/auto_correc_main.sh $FILE $ASSIGN "-13" "2932"
-if [ -e .system/grading/traceback ];then
-    mv .system/grading/traceback .
-	exit 1
-fi
-
-bash .system/auto_correc_main.sh $FILE $ASSIGN "0" "2932"
-if [ -e .system/grading/traceback ];then
-    mv .system/grading/traceback .
-	exit 1
-fi
-
-
-
 
 touch .system/grading/passed;
